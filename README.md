fb141d9f25ea4cee1f17ac6b08f273a06e96ee12
fmauri <mauri95francesco@gmail.com>
2017-10-13 17:58:45 +0200

    read me

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Moving_File
+# Sample projects:
+## Task 2
+
+Write a program which will perform the job of moving the file from one location to another.
+The source and destination path will be entered by the user.
+Perform the required error checking and handle the exceptions accordingly.
--- main.cpp
+++ main.cpp
@@ -11,7 +11,6 @@ std::string base_name(std::string const &path) {
  * Write a program which will perform the job of moving the file from one location to another.
  * The source and destination path will be entered by the user.
  * Perform the required error checking and handle the exceptions accordingly.
- * @return
  */
 int main() {
     //std::string s_from_f = "/home/mauri/Pictures/Wallpapers/test.jpg";
